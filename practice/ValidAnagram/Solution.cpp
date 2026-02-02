d198e69af1ef377cb229028c9cb01ca7ed442357
NguyenHuuHung <huuhung.9a10.2013@gmail.com>
2026-02-02 20:00:01 +0700

    is anagram

---
+++ practice/ValidAnagram/Solution.cpp
@@ -0,0 +1,25 @@
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.size() != t.size())
+            return false;
+        int charArr[26] = {0};
+        for (int i = 0; i < s.size(); i++)
+        {
+            charArr[s[i] - 'a']++;
+            charArr[t[i] + 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (charArr[i] != 0)
+                return false;
+        }
+        return true;
+    }
+}
