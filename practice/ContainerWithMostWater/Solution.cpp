53feceddbfc189010735f95d8f6bdee0f850630c
NguyenHuuHung <huuhung.9a10.2013@gmail.com>
2026-02-25 17:17:02 +0700

    container with most  water

---
+++ practice/ContainerWithMostWater/Solution.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int left = 0, right = height.size() - 1, result = 0;
+        while (left < right)
+        {
+            int area = min(height[left], height[right]) * (right - left);
+            result = max(result, area);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    vector<int> height = {1, 7, 2, 5, 12, 3, 500, 500, 7, 8, 4, 7, 3, 6};
+    Solution sl;
+
+    cout << sl.maxArea(height) << endl;
+}
