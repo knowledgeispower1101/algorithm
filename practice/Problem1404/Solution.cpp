3cf8c2b46a3dceb395b2b54c94c4a93a276a576d
NguyenHuuHung <huuhung.9a10.2013@gmail.com>
2026-02-26 08:53:14 +0700

    add problem 1404

---
+++ practice/Problem1404/Solution.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    int numSteps(string s)
+    {
+        int steps = 0;
+        int carry = 0;
+
+        for (int i = s.length() - 1; i > 0; i--)
+        {
+            int bit = s[i] - '0';
+
+            if (bit + carry == 1)
+            {
+
+                steps += 2;
+                carry = 1;
+            }
+            else
+            {
+                steps += 1;
+            }
+        }
+
+        return steps + carry;
+    }
+};
+
+int main()
+{
+    Solution s;
+    string str = "1101";
+    cout << s.numSteps(str) << endl;
+    return 0;
+}
