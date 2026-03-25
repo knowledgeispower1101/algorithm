88df7e3bed71053aa8f0ed84b36357631c3db740
NguyenHuuHung <huuhung.9a10.2013@gmail.com>
2026-03-25 08:07:33 +0700

    148A

---
+++ codeforce/148A/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int k, l, m, n, d;
+    cin >> k >> l >> m >> n >> d;
+    int result = 0;
+    for (int i = 1; i <= d; i++)
+    {
+        if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0)
+        {
+            result++;
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
