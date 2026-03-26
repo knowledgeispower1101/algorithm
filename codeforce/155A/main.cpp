d1aa8c8f7ecea6389732fd20cbca4246f0226255
NguyenHuuHung <huuhung.9a10.2013@gmail.com>
2026-03-26 16:25:52 +0700

    155A

--- codeforce/155A/main.cpp
+++ codeforce/155A/main.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int n;
     cin >> n;
-
     int x;
     cin >> x;
-
-    int minScore = x, maxScore = x;
     int count = 0;
-
+    int max_value = x, min_value = x;
     for (int i = 1; i < n; i++)
     {
         cin >> x;
-
-        if (x > maxScore)
+        if (max_value > x)
         {
+            max_value = x;
             count++;
-            maxScore = x;
         }
-        else if (x < minScore)
+        else if (min_value < x)
         {
+            min_value = x;
             count++;
-            minScore = x;
         }
     }
-
     cout << count << endl;
     return 0;
 }
