6aac0973029658bc69e0664ad23aa930b0ce51ec
NguyenHuuHung <huuhung.9a10.2013@gmail.com>
2026-03-26 17:50:25 +0700

    1899A

---
+++ codeforce/1899A/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n % 3 == 0)
+        cout << "Vova";
+    else
+        cout << "Vanya";
+
+    return 0;
+}
