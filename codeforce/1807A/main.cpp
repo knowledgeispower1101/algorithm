4c18a9116bc8ffd683bbc8c8e56c2ecd51376c51
NguyenHuuHung <huuhung.9a10.2013@gmail.com>
2026-03-28 09:26:31 +0700

    1807A

---
+++ codeforce/1807A/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a, b, c;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b >> c;
+        if (a + b == c)
+        {
+            cout << '+' << endl;
+            continue;
+        }
+        cout << '-' << endl;
+    }
+
+    return 0;
+}
