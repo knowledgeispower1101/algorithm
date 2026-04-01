14860b55c9ad306efe329f1e01790e22c72fb26f
NguyenHuuHung <huuhung.9a10.2013@gmail.com>
2026-04-01 15:48:40 +0700

    fix

--- codeforce/A/2009A/main.cpp
+++ codeforce/A/2009A/main.cpp
@@ -1,40 +1,14 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int t;
     cin >> t;
-
-    while (t--)
+    int a, b;
+    while (t-- > 0)
     {
-        int n;
-        cin >> n;
-
-        int k = n / 2;
-
-        if (k % 2 != 0)
-        {
-            cout << "NO\n";
-            continue;
-        }
-
-        cout << "YES\n";
-        int sum_even = 0;
-        for (int i = 1; i <= k; i++)
-        {
-            cout << 2 * i << " ";
-            sum_even += 2 * i;
-        }
-
-        int sum_odd = 0;
-        for (int i = 1; i < k; i++)
-        {
-            cout << 2 * i - 1 << " ";
-            sum_odd += 2 * i - 1;
-        }
-
-        cout << sum_even - sum_odd << "\n";
+        cin >> a >> b;
+        cout << (b - a) << endl;
     }
 
     return 0;
